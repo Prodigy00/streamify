88118ce7175d0053b9670bb7e8dc67b10fb10e32
Prodigy00 <simplygiddy@gmail.com>
2019-09-07 01:40:37 +0100

    created buttons for sign in and sign out state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -27,11 +27,21 @@ class GoogleAuth extends React.Component {
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>Nope, don't know if you're signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>Congrats! You are signed in!</div>;
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>Nope you're not signed in yet</div>;
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
   render() {
