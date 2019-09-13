8e10cfefe5e23e25c323d201f0731073909ad8b6
Prodigy00 <simplygiddy@gmail.com>
2019-09-13 08:05:18 +0100

    refactoring Google Auth component to use redux for managing auth state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -21,8 +23,14 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
+
+    // this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
   onSignInClick = () => {
@@ -56,4 +64,9 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+// const MapStateToProps = state => {};
+
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth);
