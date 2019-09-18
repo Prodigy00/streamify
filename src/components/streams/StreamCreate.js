2d3e5a10aa78675c90bdd169a41261f910fca9b5
Prodigy00 <simplygiddy@gmail.com>
2019-09-18 08:31:35 +0100

    added redux form to app, wokring on first form StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    // console.log(formProps, "formProps");
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+  render() {
+    console.log(this.props, "props here");
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
