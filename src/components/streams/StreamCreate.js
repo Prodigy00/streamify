4fa267d1a730022fb1518b5e13cf6900642b1a7e
Prodigy00 <simplygiddy@gmail.com>
2019-09-18 10:42:03 +0100

    created onSubmit event handler, passed it as arg to handleSubmit props for redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,16 +2,33 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input }) {
+  renderInput({ input, label }) {
     // console.log(formProps, "formProps");
-    return <input {...input} />;
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues, "form values here");
   }
   render() {
     console.log(this.props, "props here");
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
