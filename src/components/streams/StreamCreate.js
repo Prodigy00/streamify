3baebf314478d552b16266dcee34a2cb51a25cdb
Prodigy00 <simplygiddy@gmail.com>
2019-09-18 08:37:38 +0100

    destructuring formProps

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
+  renderInput({ input }) {
     // console.log(formProps, "formProps");
-    return <input {...formProps.input} />;
+    return <input {...input} />;
   }
   render() {
     console.log(this.props, "props here");
