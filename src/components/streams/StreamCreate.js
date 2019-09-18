9dc6c09f950256873d89675e76ea29783e59b2af
Prodigy00 <simplygiddy@gmail.com>
2019-09-18 12:04:52 +0100

    conditionally rendered error class from semantic ui to render input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -15,8 +15,9 @@ class StreamCreate extends React.Component {
   renderInput = ({ input, label, meta }) => {
     // console.log(formProps, "formProps");
     // console.log(meta, "meta here");
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
         {this.renderError(meta)}
